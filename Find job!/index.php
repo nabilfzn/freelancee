84d9e2b2cfa426662114c19612ce0be9f6e1be87
nabilfzn <phimenab@gmail.com>
2024-02-20 20:57:28 +0700

    commit

---
+++ Find job!/index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<section id="first">
+    <header>
+        <div class="nav">
+            <div class="logo">
+                <p></p>
+            </div>
+
+                <div class="navlist">
+                    <ul>
+                        <li><a href="../utama/index.php">Back</a></li>
+                        <li><a href="../login/index.php">Post</a></li>
+                        <li><a href="../login/index.php">LogIn</a></li>
+                    </ul>
+                </div>
+        </div>
+    </header>
+
+    <!-- halamab utama -->
+
+<div class="keseluruhan">
+    <div class="container">
+
+        <div class="search-box">
+            <form action="your-search-results-page.html" method="get">
+                <input type="text" name="keyword" placeholder="Enter keyword or location">
+                <select name="category">
+                    <option value="">Select category</option>
+                    <option value="web-development">Web Development</option>
+                    <option value="design">Design</option>
+                    <option value="marketing">Marketing</option>
+                </select>
+                <button type="submit">Search</button>
+            </form>
+            <div class="search-container">
+                <input type="text" placeholder="Search skills" class="skill-search">
+                <input type="text" placeholder="Search countries" class="country-search">
+                <div class="rating">
+                  <label for="rating-1">1</label>
+                  <label for="rating-2">2</label>
+                  <label for="rating-3">3</label>
+                  <label for="rating-4">4</label>
+                  <label for="rating-5">5</label>
+                </div>
+              </div>
+        </div>
+
+        <div class="kotak">
+            <div class="box">
+                <div class="dalam">
+                    <div class="wt">
+                        <div class="img">
+                            <img src="girl.png">
+                        </div>
+                    
+
+                        <div class="profile">
+                            <div class="name">Wong tulus</div>
+                            <div class="role">DIVI/ELEMENTOR | WORDPRESS | WIX | WEB DEVELOPER</div>
+                        </div>
+                    
+                    </div>
+                        <div class="deskripsi">
+                                <div class="skil">WordPress, HTML, PHP, JavaScript, CSS</div>
+                        
+                            <div class="bawah">
+                                <p>I am a Web Developer and Individual Freelancer. I usually create a website for clients using WordPress and custom or create add-on plugins if needed. I can create a static 
+                                and dynamic websites and I am a frontend and backend developer. I like to work on new things
+                                and learn from them as much as I can. I am always...</p>
+                                
+                                <div class="btn">
+                                    <button type = submit , class="btnd">
+                                        Hire me
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="dalam">
+                    <div class="wt">
+                        <div class="img">
+                            <img src="girl.png">
+                        </div>
+                    
+
+                        <div class="profile">
+                            <div class="name">Wong tulus</div>
+                            <div class="role">DIVI/ELEMENTOR | WORDPRESS | WIX | WEB DEVELOPER</div>
+                        </div>
+                    
+                    </div>
+                        <div class="deskripsi">
+                                <div class="skil">WordPress, HTML, PHP, JavaScript, CSS</div>
+                        
+                            <div class="bawah">
+                                <p>I am a Web Developer and Individual Freelancer. I usually create a website for clients using WordPress and custom or create add-on plugins if needed. I can create a static 
+                                and dynamic websites and I am a frontend and backend developer. I like to work on new things
+                                and learn from them as much as I can. I am always...</p>
+                                
+                                <div class="btn">
+                                    <button type = submit , class="btnd">
+                                        Hire me
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="dalam">
+                    <div class="wt">
+                        <div class="img">
+                            <img src="girl.png">
+                        </div>
+                    
+
+                        <div class="profile">
+                            <div class="name">Wong tulus</div>
+                            <div class="role">DIVI/ELEMENTOR | WORDPRESS | WIX | WEB DEVELOPER</div>
+                        </div>
+                    
+                    </div>
+                        <div class="deskripsi">
+                                <div class="skil">WordPress, HTML, PHP, JavaScript, CSS</div>
+                        
+                            <div class="bawah">
+                                <p>I am a Web Developer and Individual Freelancer. I usually create a website for clients using WordPress and custom or create add-on plugins if needed. I can create a static 
+                                and dynamic websites and I am a frontend and backend developer. I like to work on new things
+                                and learn from them as much as I can. I am always...</p>
+                                
+                                <div class="btn">
+                                    <button type = submit , class="btnd">
+                                        Hire me
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="dalam">
+                    <div class="wt">
+                        <div class="img">
+                            <img src="girl.png">
+                        </div>
+                    
+
+                        <div class="profile">
+                            <div class="name">Wong tulus</div>
+                            <div class="role">DIVI/ELEMENTOR | WORDPRESS | WIX | WEB DEVELOPER</div>
+                        </div>
+                    
+                    </div>
+                        <div class="deskripsi">
+                                <div class="skil">WordPress, HTML, PHP, JavaScript, CSS</div>
+                        
+                            <div class="bawah">
+                                <p>I am a Web Developer and Individual Freelancer. I usually create a website for clients using WordPress and custom or create add-on plugins if needed. I can create a static 
+                                and dynamic websites and I am a frontend and backend developer. I like to work on new things
+                                and learn from them as much as I can. I am always...</p>
+                                
+                                <div class="btn">
+                                    <button type = submit , class="btnd">
+                                        Hire me
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="dalam">
+                    <div class="wt">
+                        <div class="img">
+                            <img src="girl.png">
+                        </div>
+                    
+
+                        <div class="profile">
+                            <div class="name">Wong tulus</div>
+                            <div class="role">DIVI/ELEMENTOR | WORDPRESS | WIX | WEB DEVELOPER</div>
+                        </div>
+                    
+                    </div>
+                        <div class="deskripsi">
+                                <div class="skil">WordPress, HTML, PHP, JavaScript, CSS</div>
+                        
+                            <div class="bawah">
+                                <p>I am a Web Developer and Individual Freelancer. I usually create a website for clients using WordPress and custom or create add-on plugins if needed. I can create a static 
+                                and dynamic websites and I am a frontend and backend developer. I like to work on new things
+                                and learn from them as much as I can. I am always...</p>
+                                
+                                <div class="btn">
+                                    <button type = submit , class="btnd">
+                                        Hire me
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="box">
+                <div class="dalam">
+                    <div class="wt">
+                        <div class="img">
+                            <img src="girl.png">
+                        </div>
+                    
+
+                        <div class="profile">
+                            <div class="name">Wong tulus</div>
+                            <div class="role">DIVI/ELEMENTOR | WORDPRESS | WIX | WEB DEVELOPER</div>
+                        </div>
+                    
+                    </div>
+                        <div class="deskripsi">
+                                <div class="skil">WordPress, HTML, PHP, JavaScript, CSS</div>
+                        
+                            <div class="bawah">
+                                <p>I am a Web Developer and Individual Freelancer. I usually create a website for clients using WordPress and custom or create add-on plugins if needed. I can create a static 
+                                and dynamic websites and I am a frontend and backend developer. I like to work on new things
+                                and learn from them as much as I can. I am always...</p>
+                                
+                                <div class="btn">
+                                    <button type = submit , class="btnd">
+                                        Hire me
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+
+
+<footer id="contact">
+    <div class="containerf">
+        <div class="f-cont">
+            <h3>Contact Us</h3>
+            <div class="pp">
+                <p>Email : phimenab@gmail.com</p>
+                <p>phone : 08123456789</p>
+                <p>Addres : Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+        <div class="f-con">
+            <h3>Quick links</h3>
+            <ul class="list">
+                <li><a href="#first">Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#ame">About me</a></li>
+            </ul>
+        </div>
+        <div class="fcon">
+            <h3>Follow us</h3>
+            <ul>
+                <li><img src="/utama/facebook.png" alt=""><a href=""></a></li>
+                <li><img src="/utama/twitter.png" alt=""><a href=""></a></li>
+                <li><img src="/utama/instagram.png" alt=""><a href=""></a></li>
+                <li><img src="/utama/youtube.png" alt=""><a href=""></a></li>
+            </ul>
+        </div>
+        <div class="bottom-bar"> 
+        <p>&copy Findcer 2023</p>
+        </div>
+    </div>
+    </footer>        
+
+</body>
+</html>
